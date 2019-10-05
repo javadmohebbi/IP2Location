f8b9a9027452c04e2f87f851e955fb0fa8b5deb0
محمد جواد محبی <m.mohebi@MihanIns.local>
2019-10-05 13:11:47 +0330

    Add Build Script (Def Win Amd64) & Executables

---
+++ build.bat
@@ -0,0 +1,33 @@
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o ./dist/windows/amd64/ip2location.exe ./ip2location.go ./utils.go ./update.go
+
+
+SET GOOS=windows
+SET GOARCH=386
+go build -o ./dist/windows/386/ip2location.exe ./ip2location.go ./utils.go ./update.go
+
+
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o ./dist/linux/amd64/ip2location ./ip2location.go ./utils.go ./update.go
+
+
+SET GOOS=linux
+SET GOARCH=386
+go build -o ./dist/linux/386/ip2location ./ip2location.go ./utils.go ./update.go
+
+
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o ./dist/darwin/amd64/ip2location ./ip2location.go ./utils.go ./update.go
+
+
+SET GOOS=darwin
+SET GOARCH=386
+go build -o ./dist/darwin/386/ip2location ./ip2location.go ./utils.go ./update.go
+
+
+
+SET GOOS=windows
+SET GOARCH=amd64
